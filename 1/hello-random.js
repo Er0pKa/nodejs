8108485092a0dafd2781376cafd627b400a184a7
Er0pKa <yeger2021@mail.ru>
2016-07-31 19:10:46 +0300

    remove scribbletune, add beeper

--- 1/hello-random.js
+++ 1/hello-random.js
@@ -1,14 +1,10 @@
 var generator = require('random-password');
 var colors = require('colors');
-var scribble = require('scribbletune');
+var beeper = require('beeper');
 
 var min = 0,
   max = 20,
-  message = generator(Math.floor(Math.random() * (max - min + 1)) + min),
-  clip = scribble.clip({
-  notes: scribble.scale('c', 'major', 3), // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-    pattern: 'x_x_x_x_x_x_x_x_'
-  });
+  message = generator(Math.floor(Math.random() * (max - min + 1)) + min);
 
 console.log(message.random);
-scribble.midi(clip, 'cscale.mid');
+beeper('****-*-*'); // beep, beep, beep, beep, pause, beep, pause, beep 
