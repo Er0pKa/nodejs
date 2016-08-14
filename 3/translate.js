593e4c81a6f87cabb3971eeb1e38669d965f72f5
Er0pKa <yeger2021@mail.ru>
2016-08-14 19:56:18 +0300

    final third homework

--- 3/translate.js
+++ 3/translate.js
@@ -9,34 +9,29 @@ http://api.yandex.ru/translate/doc/dg/reference/translate.xml
 https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160810T160735Z.18697578f2121806.80f236aaa41fbae0a5fdb9e8f84c0bd3d00e8856&text=пчела&lang=ru-en
 trnsl.1.1.20160810T160735Z.18697578f2121806.80f236aaa41fbae0a5fdb9e8f84c0bd3d00e8856
 */
+
+
 const request = require('request'),
       http = require("http")
       url = require('url');
 
-http.createServer(onRequest).listen(8889);        //server
-console.log("Server has started.");
-
-function toYandex(wrd) {                       //word to yandex
-  var customUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160810T160735Z.18697578f2121806.80f236aaa41fbae0a5fdb9e8f84c0bd3d00e8856&text=' + wrd + '&lang=en-ru';
+function onRequest(req, res) {           //main function
+  const word = req.url.replace("/", "");
+  const apiKey = "trnsl.1.1.20160810T160735Z.18697578f2121806.80f236aaa41fbae0a5fdb9e8f84c0bd3d00e8856";
+  const customUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='
+                  + apiKey
+                  + '&text=' + word
+                  + '&lang=en-ru';
   request(customUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && res.statusCode == 200) {
       json = JSON.parse(response.body).text.toString();
-      return json;
-    }
-    else if (error) {
-      return error
+        res.write("Translate:" + json);
+        res.end();
     }
   });
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write("Translator:" + word + "\n");
+  req.setEncoding("utf8");
 }
-
-function onRequest(request, response) {           //main function
-  const word = request.url.replace("/", "");
-  const translatedWord = toYandex(word);
-  console.log('word:' + word + '\ntranslate ' + translatedWord);
-
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Translator:" + word + "\n");
-  response.write("Translate:" + translatedWord + typeof(translatedWord));
-  response.end();
-  request.setEncoding("utf8");
-}
+http.createServer(onRequest).listen(8888);        //server
+console.log("Server has started.");
